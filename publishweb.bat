8a4dfe21042f8f7ca4fd26727159874833f065bf
Tomer Rosenthal <torosent@microsoft.com>
2016-12-24 11:44:17 +0200

    Delete publishweb.bat

--- publishweb.bat
+++
@@ -1,7 +0,0 @@
-cd YInsights.Web
-dotnet publish
-cd..
-docker login torosent-microsoft.azurecr.io -u torosent -p s6k3sUB7n7+b96ffOjajBzHpXRfxPAFD
-docker tag yinsights.web torosent-microsoft.azurecr.io/yinsights.web
-docker push torosent-microsoft.azurecr.io/yinsights.web
-
